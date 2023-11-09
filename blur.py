ba660c45cf442f9957ac7dfc01dfc6abcb324c3d
David Glez <david@Davids-MacBook-Pro.local>
2023-11-09 00:58:17 -0600

    blur points

--- blur.py
+++ blur.py
@@ -3,15 +3,22 @@ import cv2 as cv
 
 import matplotlib.pyplot as plt
 
-img = cv.imread("Images/REF_23.png", cv.COLOR_BGR2GRAY)
 
-x1, y1, x2, y2 =  350, 170, 100, 135
-roi = img[y1:y1+y2, x1:x1+x2]
+# Cargar la imagen original
+img = cv.imread("Images/REF_23.png", cv.IMREAD_COLOR)  # Asegúrate de cargar la imagen en color
 
-gauss = cv.GaussianBlur(roi, (5,5), 0)
 
-canny = cv.Canny(roi, 50, 150)
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+# Definir las coordenadas del ROI
+x1, y1, x2, y2 = 350, 170, 100, 135
 
-plt.imshow(canny),plt.show()
+# Obtener la región de interés (ROI)
+roi = img_gray[y1:y1+y2, x1:x1+x2]
 
+intensity = roi[20, 40]
+print(intensity)
+
+# Mostrar la imagen original con los bordes superpuestos en la ROI
+plt.imshow(cv.cvtColor(roi, cv.COLOR_BGR2RGB))
+plt.show()
 
